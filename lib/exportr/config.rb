1eee85ff61e5d6c8fc82516ef99d406d2c5d54eb
richardcalahan <richard@alldayeveryday.com>
2012-03-30 07:03:48 -0400

    comments in config module

--- lib/exportr/config.rb
+++ lib/exportr/config.rb
@@ -2,10 +2,13 @@ module Exportr
   
   module Config
 
+    # Default config file location. 
     CONFIG_FILE     = 'config/exportr.yml'
-
+    
+    # Default error message if CLI is run from anywhere but the root
     NOT_ROOT        = 'You must run exportr from the root of your application.'
     
+    # Default error message if there is no config file present.
     NO_CONFIG_FILE  = 'You must run `rails generate exportr` first.'
 
   end
