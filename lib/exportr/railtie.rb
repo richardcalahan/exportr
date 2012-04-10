df09376804f9ca6fee8645bc17325d569a002a77
richardcalahan <richard@alldayeveryday.com>
2012-04-10 19:13:46 -0400

    env vars added before init in railtie

--- lib/exportr/railtie.rb
+++ lib/exportr/railtie.rb
@@ -4,7 +4,7 @@ module Exportr
   
   class Railtie < Rails::Railtie
 
-    initializer 'exportr.load_environment_variables' do |app|
+    config.before_initialize do
       if File.exists? "#{Rails.root}/#{Exportr::Config::CONFIG_FILE}"
         config = YAML.load(File.open("#{Rails.root}/#{Exportr::Config::CONFIG_FILE}"))
         config.each_pair { |key,value| ENV[key] = value } if config
