761909f90de1a79a177b094d8a713893455c339b
richardcalahan <richard@alldayeveryday.com>
2012-04-10 19:01:48 -0400

    moved ENV load logic to a railtie

--- lib/exportr/railtie.rb
+++ lib/exportr/railtie.rb
@@ -1,9 +1,16 @@
+require "exportr/config"
+
 module Exportr
   
   class Railtie < Rails::Railtie
+
     initializer 'exportr.load_environment_variables' do |app|
-      ENV['DUDE'] = 'sweet'
+      if File.exists? "#{Rails.root}/#{Exportr::Config::CONFIG_FILE}"
+        config = YAML.load(File.open("#{Rails.root}/#{Exportr::Config::CONFIG_FILE}"))
+        config.each_pair { |key,value| ENV[key] = value } if config
+      end
     end
+
   end
 
 end
--- lib/generators/exportr/exportr_generator.rb
+++ lib/generators/exportr/exportr_generator.rb
@@ -4,15 +4,6 @@ class ExportrGenerator < Rails::Generators::Base
 
   def generate_initializer
     copy_file 'exportr.yml', 'config/exportr.yml'
-    application '
-    # Loads export.yml and sets key=value to the local environment
-    config.before_initialize do
-      require "exportr/config"
-      if File.exists? "#{Rails.root}/#{Exportr::Config::CONFIG_FILE}"
-        config = YAML.load(File.open("#{Rails.root}/#{Exportr::Config::CONFIG_FILE}"))
-        config.each_pair { |key,value| ENV[key] = value } if config
-      end
-    end'
 
     # Decrecation warning
     if File.exists?("#{Rails.root}/config/initializers/exportr.rb")
