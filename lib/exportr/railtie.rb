2aec8444799b3cfbe8cf786167b078a58879573a
richardcalahan <richard@calahan.me>
2012-04-11 08:14:58 -0400

    wow, big sntax error fix

--- lib/exportr/railtie.rb
+++ lib/exportr/railtie.rb
@@ -8,7 +8,7 @@ module Exportr
 
     config.before_initialize do
       if File.exists? config_file
-        config = YAML.load(File.open(config_file)
+        config = YAML.load(File.open(config_file))
         config.each_pair { |key,value| ENV[key] = value } if config
       end
     end
