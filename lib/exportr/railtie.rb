03e389e9f43f234c480ea69dd4b2b6b6dbef113e
Richard Calahan <richard@calahan.me>
2013-03-14 18:28:08 -0300

    Update railtie.rb
    railtie = before configuration instead of initialize

--- lib/exportr/railtie.rb
+++ lib/exportr/railtie.rb
@@ -6,7 +6,7 @@ module Exportr
 
     extend Exportr::Base
 
-    config.before_initialize do
+    config.before_configuration do
 
       if File.exists? config_file
         config = YAML.load(File.open(config_file))
